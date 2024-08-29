1b867ed4d5d255739d10bc8a6d2b0066bf05310c
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-29 18:11:32 +0530

    export component from index.js

--- BlogApp/src/components/index.js
+++ BlogApp/src/components/index.js
@@ -1,5 +1,19 @@
 import Header from './Header/Header'
-
 import Footer from './Footer/Footer'
+import Container from './container/Container'
+import Logo from './Logo'
+import LogoutBtn from './Header/LogoutBtn'
+import Input from './Input'
+import PostCard from './PostCard'
+import Button from './Button'
 
-export {Header, Footer}
+export {
+    Header, 
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Input,
+    PostCard,
+    Button,
+}
