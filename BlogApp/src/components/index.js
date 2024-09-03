a5190bf1df9d55a16a79547f7b5c152d17178db2
Shuaib464 <shuaibshekh464@gmail.com>
2024-09-03 18:19:07 +0530

    import and export components via index.js

--- BlogApp/src/components/index.js
+++ BlogApp/src/components/index.js
@@ -6,6 +6,12 @@ import LogoutBtn from './Header/LogoutBtn'
 import Input from './Input'
 import PostCard from './PostCard'
 import Button from './Button'
+import AuthLayout from './AuthLayout'
+import RTE from './RTE'
+import Signup from './Signup'
+import Login from './Login' 
+import PostForm from './post-form/PostForm'
+import Select from './Select'
 
 export {
     Header, 
@@ -16,4 +22,10 @@ export {
     Input,
     PostCard,
     Button,
+    AuthLayout,
+    RTE,
+    Signup, 
+    Login,
+    PostForm,
+    Select,
 }
